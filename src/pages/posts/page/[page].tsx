31d6890142c249f821a7b9fa4051f2763c67272b
peterjacobson <peterjacobson.nz@gmail.com>
2021-07-23 10:21:27 +1200

    fix titles

--- src/pages/posts/page/[page].tsx
+++ src/pages/posts/page/[page].tsx
@@ -20,7 +20,7 @@ type Props = {
 };
 export default function Page({ posts, tags, pagination, page }: Props) {
   const url = `/posts/page/${page}`;
-  const title = "All posts";
+  const title = "Home";
   return (
     <Layout>
       <BasicMeta url={url} title={title} />
